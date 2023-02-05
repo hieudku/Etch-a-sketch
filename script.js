0efb1f7ac79596dc90c31a7c03fc5e3dbd75543f
Hieu Cu <hieu.dcu@gmail.com>
2023-02-05 22:40:47 +1300

    Removed prompt, added input box to return user's input value

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-/* Ask for user's input on the number of boxes,
-and makes boxes appear or disappear. */
-
-
-let userInput = prompt('Select from 10x10 to 100x100');
-
-/* Create and toggle grid boxes. Use loop?*/
+/* Ask for user's input on the number of boxes horizontally.
+ */
+let userInput;
+let input = document.getElementById("myInput");
+    input.addEventListener('keyup', function(event) {
+        if (event.key === "Enter") {
+             userInput = this.value;
+        }
+/* Create grid boxes. Use loop?*/
 let mouseDown = false;
-const totalBoxes = userInput ** 2;
+const totalBoxes = parseInt(userInput) ** 2;
 for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equal div boxes.
     const gridWrapper = document.getElementById("gridBox");
     const createdBox = document.createElement("div");
         createdBox.classList.add("box");
         gridWrapper.appendChild(createdBox)
             console.log('test ' + i);
-            /* On hover effect when mouse passes over. */
+            /* Click and hold for effect when mouse passes over. */
         createdBox.addEventListener("mousedown", function() {
             mouseDown = true;
         })
@@ -26,10 +28,13 @@ for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equa
             }
         })
 }
-
 /* Set width of the parent div box to always proportional to the user input */
 let gridBoxSize = document.getElementById("gridBox");
-    gridBoxSize.style.width = userInput * 10 + "px";
+    gridBoxSize.style.width = parseInt(userInput) * 10 + "px";
+    gridBoxSize.style.height = parseInt(userInput) * 10 + "px";
+    }) /* wrap the click button function around the loop and other listeners
+        in order to wait for user's input parsed first before loading the grids */
+
 
 
 
