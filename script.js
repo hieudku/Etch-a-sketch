909bcbf98e13ac84dc070afef8c3d81610ffb2a0
Hieu Cu <hieu.dcu@gmail.com>
2023-02-05 23:43:31 +1300

    Added function for reset button, wrapped userInput loop function inside createGrids function

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
-/* Ask for user's input on the number of boxes horizontally.
- */
+/* wrap the click button function around the loop and other listeners
+    in order to wait for user's input parsed first before loading the grids */
 let userInput;
-let input = document.getElementById("myInput");
+let input = document.getElementById("myInput"); // input box
     input.addEventListener('keyup', function(event) {
         if (event.key === "Enter") {
              userInput = this.value;
         }
+        
+/* Remove grids upon user's next input using while loop. */
+const removeGrids = document.getElementById("gridBox");
+while (removeGrids.firstChild) {
+    removeGrids.removeChild(removeGrids.firstChild);
+}
+/* function for reset button */
+let reset = document.getElementById("resetBtn"); // reset button
+    reset.addEventListener('click', () => {
+    const resetButton = document.getElementById("gridBox");
+    while (resetButton.firstChild) {
+    resetButton.removeChild(resetButton.firstChild);
+    }
+    createGrids();
+})
+createGrids();
 /* Create grid boxes. Use loop?*/
+function createGrids () {
 let mouseDown = false;
 const totalBoxes = parseInt(userInput) ** 2;
 for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equal div boxes.
@@ -14,8 +31,7 @@ for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equa
     const createdBox = document.createElement("div");
         createdBox.classList.add("box");
         gridWrapper.appendChild(createdBox)
-            console.log('test ' + i);
-            /* Click and hold for effect when mouse passes over. */
+/* Click and hold for effect when mouse passes over. */
         createdBox.addEventListener("mousedown", function() {
             mouseDown = true;
         })
@@ -28,13 +44,11 @@ for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equa
             }
         })
 }
+}
 /* Set width of the parent div box to always proportional to the user input */
 let gridBoxSize = document.getElementById("gridBox");
     gridBoxSize.style.width = parseInt(userInput) * 10 + "px";
-    gridBoxSize.style.height = parseInt(userInput) * 10 + "px";
-    }) /* wrap the click button function around the loop and other listeners
-        in order to wait for user's input parsed first before loading the grids */
-
+    }) // end of button click event listener function.
 
 
 
