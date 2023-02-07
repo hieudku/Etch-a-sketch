4f999f5f471935a62517c72604dfe37c5015dd11
Hieu Cu <hieu.dcu@gmail.com>
2023-02-07 14:31:32 +1300

    moved function call createGrid inside keyup event, fixed createdBox DOM method to fit inside gridBox

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 /* wrap the click button function around the loop and other listeners
     in order to wait for user's input parsed first before loading the grids */
-    let userInput = 0;
+    let userInput = 0; // global scope
     let input = document.getElementById("myInput"); // input box
     input.addEventListener('keyup', function(event) {
         if (event.key === "Enter") {
-            var inputValue = Number(this.value);
+            var inputValue = Number(this.value); // assign and convert input into number
             if (inputValue >= 10 && inputValue <= 100) {
-                userInput = inputValue;
+                userInput = inputValue; // if both ifs are true assign value to global scope as number to be used elsewhere
             }
-        }
+        
 
 /* Remove grids upon user's next input using while loop. */
 const removeGrids = document.getElementById("gridBox");
@@ -16,37 +16,37 @@ while (removeGrids.firstChild) {
     removeGrids.removeChild(removeGrids.firstChild);
 }
 createGrids(); // create grids when user enter input
+        }
+}); // end of Enter keyup eventlistener function.
 
-/* Set width of the parent div box to always proportional to the user input */
-let gridBoxSize = document.getElementById("gridBox");
-    gridBoxSize.style.width = parseInt(userInput) * 10 + "px";
-}) // end of Enter keyup eventlistener function.
 
-/* Create grid boxes. Use loop */
-function createGrids () {
+function createGrids () { /* Create grid boxes and fill color with mouse events */
     let mouseDown = false;
     const totalBoxes = parseInt(userInput) ** 2;
     for(i = 0; i < totalBoxes; i++) { // Loop through the input and display the equal div boxes.
         const gridWrapper = document.getElementById("gridBox");
-        const createdBox = document.createElement("div");
+        let createdBox = document.createElement("div");
             createdBox.classList.add("box");
-            gridWrapper.appendChild(createdBox)
+                console.log('Total boxes: ' + totalBoxes);
+            gridWrapper.appendChild(createdBox);
+            createdBox.style.width = parseInt(gridWrapper.offsetWidth / userInput) + "px";
+            createdBox.style.heigth = parseInt(gridWrapper.offsetHeight / userInput) + "px";
+
 /* Click and hold for effect when mouse passes over. */
             createdBox.addEventListener("mousedown", function() {
                 mouseDown = true;
                 createdBox.style.backgroundColor = 'rgb(189, 60, 56)';
-            })
+            });
             createdBox.addEventListener("mouseup", function() {
                 mouseDown = false;
-            })
+            });
             createdBox.addEventListener("mousemove", function() {
                 if (mouseDown) {
-                createdBox.style.backgroundColor = 'rgb(189, 60, 56)';
+                    createdBox.style.backgroundColor = 'rgb(189, 60, 56)';
                 }
-            })
+            });
     }
 }
-
 /* function for reset button */
 let reset = document.getElementById("resetBtn"); // get reset button
     reset.addEventListener('click', () => {
